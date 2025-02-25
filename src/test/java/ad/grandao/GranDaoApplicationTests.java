8ec6307dc218b75a33d748901171a7ed8a94d5cc
g4vr3 <gdfizan@gmail.com>
2025-02-25 13:44:33 +0100

    Initial commit

---
+++ src/test/java/ad/grandao/GranDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.grandao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
