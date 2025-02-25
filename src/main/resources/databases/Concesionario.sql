5abc43b4889f6c71c2d211b1c490a8a9e14361a2
javier-agudo-fernandez <javier.agudo1@educa.madrid.org>
2025-02-25 16:58:14 +0100

    Create db scripts

--- src/main/resources/databases/Concesionario.sql
+++ src/main/resources/databases/Concesionario.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS Concesionario;
 USE Concesionario;
 
-CREATE TABLE IF NOT EXISTS coches (
+CREATE TABLE IF NOT EXISTS coche (
                                       matricula VARCHAR(15) PRIMARY KEY,
                                       marca VARCHAR(50) NOT NULL,
                                       modelo VARCHAR(50) NOT NULL,
