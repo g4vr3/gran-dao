68a24168314ad3f491dd8deab5b0277bad647f46
g4vr3 <gdfizan@gmail.com>
2025-02-25 16:34:52 +0100

    Initial commit

---
+++ src/main/resources/databases/Concesionario.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS Concesionario;
+USE Concesionario;
+
+CREATE TABLE IF NOT EXISTS coches (
+    matricula VARCHAR(15) PRIMARY KEY,
+    marca VARCHAR(50) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    color VARCHAR(30) NOT NULL
+    );
