ac6db21697d0bf8410674ac3b8dc149b0b9ddff7
g4vr3 <gdfizan@gmail.com>
2025-03-01 15:36:39 +0100

    Add cacheConfig to bicicletas

--- src/main/java/ad/grandao/controller/BicicletaController.java
+++ src/main/java/ad/grandao/controller/BicicletaController.java
@@ -4,11 +4,14 @@ import ad.grandao.model.Bicicleta;
 import ad.grandao.service.BicicletaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bicicletas")
+@CacheConfig(cacheNames = "bicicletas")
 public class BicicletaController {
 
     private final BicicletaService bicicletaService;
@@ -26,6 +29,7 @@ public class BicicletaController {
 
     // Obtener una bicicleta por su ID
     @GetMapping("/{id}")
+    @Cacheable
     public ResponseEntity<?> getBicicleta(@PathVariable String id) {
         return ResponseEntity.ok(bicicletaService.findById(id));
     }
